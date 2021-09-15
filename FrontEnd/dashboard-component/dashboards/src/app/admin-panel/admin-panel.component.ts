e88b91530a1419037f71e21db6c9fcc252f48629
Jonah Valdez <valdezjonah111@gmail.com>
2021-09-15 16:36:32 -0500

    added the create employee in admin panel

--- FrontEnd/dashboard-component/dashboards/src/app/admin-panel/admin-panel.component.ts
+++ FrontEnd/dashboard-component/dashboards/src/app/admin-panel/admin-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-panel',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-panel.component.css']
 })
 export class AdminPanelComponent implements OnInit {
-
+    loginRef1 = new FormGroup({
+    user:new FormControl(),
+    pass:new FormControl()
+  });
   constructor() { }
 
   ngOnInit(): void {
+  }
+  createEmployee(){
+    let login = this.loginRef1.value;
+    let user = login.user;
+    let pass = "welcome123"; 
+    console.log(user);
+    console.log(pass); 
   }
 
 }
