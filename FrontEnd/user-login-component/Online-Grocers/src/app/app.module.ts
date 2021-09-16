e0452874884abea72dfc93c609e8a5b0ac525ebb
munjaap123 <munjaap.uppal@tcs.com>
2021-09-15 20:48:55 -0400

    Login and Product Components

--- FrontEnd/user-login-component/Online-Grocers/src/app/app.module.ts
+++ FrontEnd/user-login-component/Online-Grocers/src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProductsComponent } from './products/products.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
